8ca84ef7d7a98c2d4e3792c21318760c0b839c8c
evox1994 <dmitrybatukhtin@gmail.com>
2018-04-15 21:40:17 +0700

    sdfgh

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 
+    function anim_add() {
+        $('.anim-parent').addClass("active");
+    }
+    function anim_remove() {
+        $('.anim-parent').removeClass("active");
+    }
+
     $(window).scroll(function(){
         var st = $(this).scrollTop();
+        var pos = $('.anim-parent').offset().top - 200;
         
         if ( $(window).width() > 768 ) {
             if ( st > 90 ) {
@@ -30,6 +38,11 @@ $(document).ready(function(){
                 }
             }
         }*/
+        if ( st > pos ) {
+            anim_add();
+        } else {
+            anim_remove();
+        }
     });
 
     $('.fancybox').fancybox();
